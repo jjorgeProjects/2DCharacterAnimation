3c85a1e4b83ffc7fa14be8f3ed5db830fd2ebd6e
jjorgeProjects <jjorge@florida-uni.es>
2025-11-24 22:57:55 +0100

    Included camera movement

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -2,16 +2,24 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    public GameObject player;
+    public Vector3 offset;
+    public float smoothing = 5f;
+    public Transform target;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //Offset between camera position and player position
+        offset = transform.position - target.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.position = player.GetComponent<Transform>().position;
+        //transform.position = target.position + offset;
+
+        //Include LERP
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, smoothing * Time.deltaTime);
+
     }
 }
